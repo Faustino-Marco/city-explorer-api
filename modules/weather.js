3bc32a214e14344ad0f5bc508280f5bd06f40b1f
Faustino Marco Simpliciano <marco.simpliciano@gmail.com>
2022-07-04 19:38:56 -0700

    integrated cache code into weather.js

--- modules/weather.js
+++ modules/weather.js
@@ -1,20 +1,42 @@
 'use strict';
 
 const axios = require('axios');
+const cache = require('./cache');
 
 async function getWeather(request, response) {
   let searchLat = request.query.lat;
   let searchLon = request.query.lon;
+  let key = 'weather-' + searchLat + searchLon;
   let url = `http://api.weatherbit.io/v2.0/forecast/daily?lat=${searchLat}&lon=${searchLon}&key=${process.env.WEATHER_BIT_IO_API_KEY}&days=5&units=I&lang=en`
-  let dataFromWeatherAPI = await axios.get(url);
+
+  if (cache[key] && (Date.now() - chace[key].timestamp < 50000)) {
+    console.log('cache hit');
+  } else {
+    let dataFromWeatherAPI = await axios.get(url);
+    console.log('Cache miss');
+    cache[key] = {};
+    cache[key].timestamp = Date.now();
+    cache[key].data = dataFromWeatherAPI.then(response => parseWeather(response.data));
+  }
+  return cache[key].data;
+}
   // console.log(dataFromWeatherAPI.data);
+function parseWeather(weatherStats) {
+  try {
+    const weatherSummaries = weatherStats.data.map(day => new Forecast(day));
+    return Promise.resolve(weatherSummaries);
+  } catch (e) {
+    return Promise.reject(e);
+  }
+}
 
-  let retrievedForecastData = dataFromWeatherAPI.data.data.map(day => new Forecast(day));
 
-  console.log(retrievedForecastData);
+//   let retrievedForecastData = dataFromWeatherAPI.data.data.map(day => new Forecast(day));
 
-  response.status(200).send(retrievedForecastData);
-};
+//   console.log(retrievedForecastData);
+
+//   response.status(200).send(retrievedForecastData);
+// };
 
 //CLASSES
 class Forecast {
